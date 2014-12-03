147c0a19a6c0063c6ef021a75f6f1465c10e33ce
Duncan Graham Walker <walkerdu@bc.edu>
2014-12-02 19:30:23 -0500

    Script concatenation PoC

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 /* jshint node: true */
 'use strict';
 
+var concat = require('broccoli-concat');
+var mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-cli-concat',
-  fileName: 'app',
+  outputDir: 'assets',
+  outputFileName: 'app',
   shouldConcatFiles: false,
+
+  /**
+  Append `<style>` and `<script>` tags to the app's html file (index.html by default) to load only the script we require.
+
+  @method contentFor
+  @param String type
+  */
 
   contentFor: function(type) {
     var _this = this;
@@ -24,7 +35,7 @@ module.exports = {
       var ext = type === 'script' ? 'js' : 'css';
 
       if (_this.shouldConcatFiles) {
-        return asset(type, 'assets/' + _this.fileName + '.' + ext);
+        return asset(type, 'assets/' + _this.outputFileName + '.' + ext);
       } else {
         /* Passes something like: asset('script', 'assets/vendor.js') */
         return asset(type, paths.vendor[ext]) + asset(type, paths.app[ext]);
@@ -43,7 +54,7 @@ module.exports = {
   included: function(app) {
     this.app = app;
 
-    if (app.env.toString() === 'development') {
+    if (app.env.toString() !== 'development') { // CHANGE BACK TO === after dev
 
     } else {
       this.shouldConcatFiles = true;
@@ -51,11 +62,19 @@ module.exports = {
   },
 
   postprocessTree: function(type, tree) {
-    console.log('------------------------------------');
-    console.log(type);
-    console.log(tree);
+    var paths = this.app.options.outputPaths;
 
-    return tree;
+    var concatenatedScripts = concat(tree, {
+      allowNone: true,
+      inputFiles: [paths.vendor.js, paths.app.js],
+      outputFile: this.outputDir + '/' + this.outputFileName + '.js';
+      // header: '/* Concatenated using https://github.com/sir-dunxalot/ember-cli-concat */',
+      wrapInFunction: false,
+    });
+
+    var workingTree = mergeTrees([tree, concatenatedScripts]);
+
+    return workingTree;
   }
 
   // treeFor: function() {
