4d1e9c075f8869d8996e1df203ab1e0a36854cfd
Petr Kosikhin <mq3_storm@mail.ru>
2016-12-09 03:55:03 +0300

    support 2.10

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 /* Dependencies */
 
-var concatAndMap = require('broccoli-sourcemap-concat');
+var concatAndMap = require('broccoli-concat');
 var fileRemover = require('broccoli-file-remover');
 var mergeTrees = require('broccoli-merge-trees');
 
@@ -16,18 +16,18 @@ Example usage:
 var path = defaultFor(path, 'assets/app.js');
 ```
 
-@method defaultFor
-@param {all} The variable you want to use the value of but need a fallback
-@param {all} The default value to use in cases where the value of `variable` is not defined
-*/
-
+ * @method defaultFor
+ * @param variable {all} The variable you want to use the value of but need a fallback
+ * @param defaultValue {all} The default value to use in cases where the value of `variable` is not defined
+ * @returns {all}
+ */
 var defaultFor = function(variable, defaultValue) {
   if (typeof variable !== 'undefined' && variable !== null) {
     return variable;
   } else {
     return defaultValue;
   }
-}
+};
 
 /* The main event woop woop */
 
@@ -145,7 +145,7 @@ module.exports = {
   ```
 
   @method cleanPath
-  @param {String} The path to clean up
+  @param path {String} The path to clean up
   */
 
   cleanPath: function(path) {
@@ -157,69 +157,36 @@ module.exports = {
   The contentFor hook is run once for each `content-for` in our application. `head` and `body`, which are the two we hook onto, are standard to Ember CLI builds and are found in the app's main HTML filem which is `app/index.html` by default.
 
   @method contentFor
-  @param {String} type The type of content-for this is being run for (e.g. head, body, etc)
+  @param contentForType {String} type The type of content-for this is being run for (e.g. head, body, etc)
   */
 
   contentFor: function(contentForType) {
-    if (!this.enabled) {
-      return;
-    } else if (contentForType === this.js.contentFor) {
-      return this.getTags('js');
-    } else if (contentForType === this.css.contentFor) {
-      return this.getTags('css');
-    } else {
-      return;
+    if (this.enabled) {
+      if (contentForType === this.js.contentFor) {
+        return [this.appPath('js')];
+      } else if (contentForType === this.css.contentFor) {
+        return [this.appPath('css')];
+      }
     }
   },
 
-  filterAndCleanPaths: function(ext, requireOriginalPaths) {
-    return this.filterPaths(ext, requireOriginalPaths).map(function(path) {
+  appPath(ext, relPath) {
+    var path;
+    if (ext === 'css') {
+      path = this._outputPaths['app'][ext]['app'];
+    } else {
+      path = this._outputPaths['app'][ext];
+    }
+    if (relPath) {
       return this.cleanPath(path);
-    }.bind(this));
-  },
-
-  filterPaths: function(ext, requireOriginalPaths) {
-    var outputPaths = this._outputPaths;
-    var filteredPaths = [];
-    var typeOptions = this[ext];
-    var addPath, concatPath;
-
-    requireOriginalPaths = defaultFor(requireOriginalPaths, false);
-
-    /* Build array in custom order so each tag is
-    in the correct order */
-
-    addPath = function(path) {
-      /* Don't include test assest in concatination */
-
-      if (path.indexOf('test') > -1) {
-        return;
-      } else {
-        filteredPaths.unshift(path);
-      }
-    }.bind(this);
-
-    if (typeOptions.concat && !requireOriginalPaths) {
-      concatPath = '/' + this.outputDir + '/' + this.outputFileName;
-
-      addPath(concatPath + '.' + ext);
     } else {
-      for (var treeName in outputPaths) {
-        var assets = outputPaths[treeName];
-        var paths = assets[ext];
-        var path;
-
-        if (typeof paths === 'string') {
-          addPath(paths);
-        } else {
-          for (var type in paths) {
-            addPath(paths[type]);
-          }
-        }
-      }
+      return this.getAssetTag(ext, path);
     }
 
-    return filteredPaths;
+  },
+
+  vendorPath(ext) {
+    return this.cleanPath(this._outputPaths['vendor'][ext]);
   },
 
   getAssetTag: function(ext, path) {
@@ -234,12 +201,6 @@ module.exports = {
     }
   },
 
-  getTags: function(ext) {
-    return this.filterPaths(ext).map(function(path) {
-      return this.getAssetTag(ext, path);
-    }.bind(this));
-  },
-
   /**
   Overrides this addon's default options with any specified by the developer and determines whether or not to concatenate files based on the environment. Please note, there is a fallback check for detecting test environments in the contentFor hook.
   The included hook is run once during the build process of the addon.
@@ -249,7 +210,6 @@ module.exports = {
   */
 
   included: function(app) {
-    var environment = app.env.toString();
     var options = defaultFor(app.options.emberCliConcat, {});
 
     this._outputPaths = app.options.outputPaths;
@@ -276,14 +236,13 @@ module.exports = {
   */
 
   postprocessTree: function(type, tree) {
-    if (this.treeTypes.indexOf(type) != -1 || this.treeTypes.length === 0) {
+    if ((this.treeTypes.indexOf(type) !== -1 ||
+      this.treeTypes.length === 0) &&
+      this.enabled
+    ) {
       var outputPath = '/' + this.cleanPath(this.outputDir) + '/' + this.outputFileName;
       var cssOptions = this.css;
       var jsOptions = this.js;
-
-      if (!this.enabled) {
-        return tree;
-      }
 
       var concatenatedScripts, concatenatedStyles, removeFromTree, scriptInputPaths, styleInputPaths, trees, workingTree;
 
@@ -296,12 +255,13 @@ module.exports = {
       /* Locate all script files and concatenate into one file */
 
       if (jsOptions.concat) {
-        scriptInputPaths = this.filterAndCleanPaths('js', true);
+        scriptInputPaths = [this.appPath('js', true)];
 
         concatenatedScripts = concatAndMap(tree, {
           allowNone: true,
           inputFiles: scriptInputPaths,
           outputFile: outputPath + '.js',
+          headerFiles: [this.vendorPath('js')],
           footer: jsOptions.footer,
           header: jsOptions.header,
           wrapInFunction: this.wrapScriptsInFunction
@@ -315,7 +275,7 @@ module.exports = {
       /* Locate all style files and concatenate into one file */
 
       if (cssOptions.concat) {
-        styleInputPaths = this.filterAndCleanPaths('css', true);
+        styleInputPaths = [this.appPath('css', true), this.vendorPath('css')];
 
         concatenatedStyles = concatAndMap(tree, {
           allowNone: true,
